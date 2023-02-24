aab0b21c62f7deaa0d2cfd2219a6854117ba7981
Ameya <ameyadali99@gmail.com>
2023-02-24 21:50:08 +0530

    Added A Pattern Using NestedFor

--- NestedFor.java
+++ NestedFor.java
@@ -1,6 +1,7 @@
 public class NestedFor {
   public static void main(String args[])
   {
+      /*
       for(int i=1; i<=10; i++)
           {
               for(int j=1; j<=5; j++)
@@ -9,5 +10,15 @@ public class NestedFor {
                   }
           }
       System.out.println("All Iterations Ended!");
+      */
+
+      for(int i=1; i<=6; i++)
+          {
+              for(int j=1; j<=i; j++)
+                  {
+                      System.out.print("*");
+                  }
+              System.out.print("\n");
+          }
   }
 }
